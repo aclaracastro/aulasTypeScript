fb39e851ee6a8e0949afc5cb376720e3bc703e9e
Ana Clara Castro <103144032+aclaracastro@users.noreply.github.com>
2024-12-10 22:28:11 -0300

    primeira aula

---
+++ aulas/dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+const add = (a, b) => {
+    return a + b;
+};
+const n1 = 15;
+const n2 = 10;
+const result = add(n1, n2);
+console.log(`a soma de ${n1} e ${n2} é ${result}`);
+const ana = {
+    name: 'ana',
+    age: null,
+    weight: '71kg',
+    isStudent: true
+};
+console.log(ana);
